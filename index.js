2084200ac7e77a4068342dbdc71114f6af618573
Bilooty <bill.j.rule@gmail.com>
2022-05-17 00:00:32 +1000

    added cats

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ fetch('https://api.kanye.rest')
 .then(quote => {
     quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
 })
+
+let catButton = document.getElementById('give-cat')
+catButton.addEventListener("click", evt => {
+    let catDiv = document.getElementById('cat-pic')
+    fetch('https://api.thecatapi.com/v1/images/search?')
+    .then(res => res.json())
+    .then(cats => {
+        cats.forEach(cat => {
+            catDiv.innerHTML = `<h3>Here is a cat, idiot<h3>
+            <img src="${cat.url}" alt="kitty"/>`
+
+        });
+    })
+})
